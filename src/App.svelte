b0fe841ccbdfb49b92a47f6215206fe85a4c8597
Abhay Nath <abhaynath88@gmail.com>
2022-08-23 23:43:15 +0530

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+</script>
+
+<div>
+  <h1>Tetris</h1>
+</div>
+
+<style>
+  h1 {
+    color: green;
+  }
+</style>
---
+++ tests/common.test.ts
@@ -0,0 +1,6 @@
+describe("testing", () => {
+  it("1+1=2", () => {
+    expect(2).toBe(2);
+  });
+});
+export {}
